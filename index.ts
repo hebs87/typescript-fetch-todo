58356c68c227a2c6b2ba9f1a16f9085283661e8c
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-05 23:06:43 +0000

    Created simple TS app to fetch and display data from API call

---
+++ index.ts
@@ -0,0 +1,32 @@
+/* A simple TS app to fetch and display data from an API call */
+
+import axios from "axios";
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// This allows us to specify the data types for each property that we are receiving
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+        The todo with ID: ${id}
+        Has a title of: ${title}
+        Is it finished: ${completed}
+    `);
+};
+
+axios.get(url)
+    .then(res => {
+        // We format the data as the interface so that we can error handle
+        const todo = res.data as Todo;
+
+        const id = todo.id;
+        const title = todo.title;
+        const completed = todo.completed;
+
+        logTodo(id, title, completed);
+    });
