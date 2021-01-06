dd9b203c9c0f7a057cb6468a697d0be0bb92dab8
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-06 22:24:01 +0000

    Added file with variable annotation examples

---
+++ annotations/variables.ts
@@ -0,0 +1,17 @@
+// Assign type of number to the variable
+let apples: number = 5;
+
+// Assign type of string to the variable
+let speed: string = 'fast';
+
+// Assign type of boolean to the variable
+let hasName: boolean = true;
+
+// Assign type of null to the variable
+let nothingMuch: null = null;
+
+// Assign type of undefined to the variable
+let nothing: undefined = undefined;
+
+// Assign type of Date - built in object - to the variable
+let now: Date = new Date();
