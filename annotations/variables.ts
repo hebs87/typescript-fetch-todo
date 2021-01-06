b7a3bd3573e57343fa9d5768cc20a9b5360ec93f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-06 22:31:57 +0000

    Added array and object annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -15,3 +15,19 @@ let nothing: undefined = undefined;
 
 // Assign type of Date - built in object - to the variable
 let now: Date = new Date();
+
+// Assign type of Array of strings to the variable
+let colors: string[] = ['red', 'green', 'blue'];
+
+// Assign type of Array of numbers to the variable
+let numbers: number[] = [1, 2, 3];
+
+// Assign type of classes to the variable - we specify the class name, so the variable will be an instance of it
+class Car {}
+let car: Car = new Car();
+
+// Assign type of object literal to the variable
+let point: { x: number, y: number } = {
+    x: 20,
+    y: 20,
+};
