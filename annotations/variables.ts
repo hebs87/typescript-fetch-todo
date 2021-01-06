8ecf56df44ea4d9c3222be81578b5cb895678d71
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-06 22:54:22 +0000

    Added examples of when to use annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -37,3 +37,29 @@ let point: { x: number, y: number } = {
 const logNumber: (i: number) => void = (i:number) => {
     console.log(i);
 };
+
+// When to use annotations
+// 1) When a function returns an 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: {x: number, y: number} = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20};
+
+// 2) When we declare a variable on one line and initialise it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+
+// 3) When we have a variable whose type can't be inferred correctly
+let moreNumbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < moreNumbers.length; i++) {
+    if (moreNumbers[i] > 0) {
+        numberAboveZero = moreNumbers[i];
+    }
+}
